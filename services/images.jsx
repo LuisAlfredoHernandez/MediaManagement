a73c843c0ea07158a9d395edb02fc1367222c407
Luis Hernandez <luiszah36@gmail.com>
2023-06-16 11:33:38 -0400

    Adding getResources with params  service

--- services/images.jsx
+++ services/images.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const getResources = async (desde = 1, limite = 10) => {
   try {
-    const response = await axios.get(`http://localhost:8080/api/resource`, {
+    const response = await axios.get(`https://mmrestfullapi-production.up.railway.app/api/resource`, {
       params: {
         desde,
         limite
@@ -18,18 +18,18 @@ const getResources = async (desde = 1, limite = 10) => {
 
 
 const getResourcesByDropDown = async (inputValue, DropDownValue) => {
-  // try {
-  //   const response = await axios.get(`http://localhost:8080/api/resource`, {
-  //     params: {
-  //       desde,
-  //       limite
-  //     }
-  //   })
-  //   if (response.status === 200)
-  //     console.log(response)
-  // } catch (error) {
-  //   console.log(error)
-  // }
+  try {
+    const response = await axios.get(`http://localhost:8080/api/resource`, {
+      params: {
+        DropDownValue,
+        inputValue
+      }
+    })
+    if (response.status === 200)
+      console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
